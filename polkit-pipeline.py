e8f24eefa4908e345ba52ff6f0b55b5ab9616e97
himozzza <89920794+himozzza@users.noreply.github.com>
2022-02-09 11:43:32 +0300

    Add files via upload

--- polkit-pipeline.py
+++ polkit-pipeline.py
@@ -10,15 +10,18 @@ now = datetime.datetime.today().strftime("%Y%m%d")
 bash("wget https://www.freedesktop.org/software/polkit/releases/")
 with open("index.html", "r") as htmlfile:
     polkit_html = htmlfile.readlines()
-gitversion = str(polkit_html[-6].split('-')[1].split(".")[0]) + "." + str(polkit_html[-6].split('-')[1].split(".")[1])
-bash("emerge -s sys-auth/polkit | grep ' Latest version available:' > polkit_installed")
-with open("polkit_installed") as file:
+gitversion = str(polkit_html[-6].replace('polkit-', '~').split("~")[1].replace('.tar.gz">', ''))
+bash("eix sys-auth/polkit | grep 'Available versions:' > polkit_latest")
+with open("polkit_latest") as file:
     polkit_file = file.readlines()
-last_version = str(polkit_file[0].split(" ")[-1].split("\n")[-2])
+last_version = str(polkit_file[0].replace("-", "~").split("~")[-2])
 
 if gitversion > last_version:
-    bash("cp polkit-{}.ebuild polkit-{}-p{}.ebuild".format(last_version, gitversion, now))
-    bash("sudo ebuild polkit-{}-p{}.ebuild digest".format(gitversion, now))
+    bash("cp polkit-{}.ebuild polkit-{}-r9.ebuild".format(last_version, gitversion))
+    bash("sudo ebuild polkit-{}-r9.ebuild digest".format(gitversion))
+    print("\n")
+    print("ebuild update")
 else:
     print("No updates available")
-bash("rm index* polkit_installed")
+
+bash("rm index* polkit_latest")
